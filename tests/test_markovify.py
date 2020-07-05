9515fd5e5b49ab8902685acc58994a076ef87dfe
Gabriele Modena <gm@nowave.it>
2020-07-06 00:53:33 +0200

    Fix. duplicated test function name.

--- tests/test_markovify.py
+++ tests/test_markovify.py
@@ -6,11 +6,11 @@ def test_invalid_init(corpus):
     with pytest.raises(KeyError):
         bot = MarkovifyAdapter(text="")
 
-
-def test_invalid_init(corpus):
     with pytest.raises(KeyError):
         bot = MarkovifyAdapter(text=corpus, language=None)
 
+        with pytest.raises(KeyError):
+            bot = MarkovifyAdapter(text=corpus, language='unknown language')
 
 def test_markovify_adapter(corpus):
     bot = MarkovifyAdapter(text=corpus)
